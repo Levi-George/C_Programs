1a90f59b59720555e1b050af3327d05afda0c720
LG <lmeg00001@gmail.com>
2022-11-14 14:44:05 -0500

    Processor Error Cleanup

--- Dining_Philosophers/DiningSoln2.c
+++ Dining_Philosophers/DiningSoln2.c
@@ -6,6 +6,7 @@
 #include <time.h>
 
 
+
 const int MAX_MEALS = 10; //maximum number of meals each philosopher may partake in
 const int NUM_OF_PHIL = 5;
 const double MAX_ACTIVE_TIME = 4;
@@ -21,15 +22,16 @@ sem_t chopstick[5]; //chopsticks for eating, semaphores for food access
 int totalMeals[5]; // times each philosopher eats
 pthread_t philThreads[5];
 
-int meals = 0;
+int meals = 0; // have we reached 10 meals?
+sem_t mealAccess;
 
 pthread_mutex_t P_StatusMutex;
 pthread_cond_t P_StatusCond[5];
 
 
-int philosopher();
-int pickup_forks(int);
-int return_forks(int);
+void *philosopher(void *arg);
+void pickup_forks(int*);
+void return_forks(int*);
 
 
 int main(double time_Param)
@@ -44,9 +46,10 @@ int main(double time_Param)
 
     //initialize our mutex and condition variable
     pthread_mutex_init(&P_StatusMutex, NULL);
+    sem_init(&mealAccess, 0, 1);
 
     //initialize our semaphores
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < NUM_OF_PHIL; i++)
     {
         sem_init(&chopstick[i], 0, 1);
         
@@ -55,27 +58,27 @@ int main(double time_Param)
     
 
     //create 5 philosopher threads
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < NUM_OF_PHIL; i++)
     {
-        pthread_create(&philThreads[i],NULL,philosopher(i),NULL); //what var do we send
+        pthread_create(&philThreads[i],NULL,&philosopher,&i); //what var do we send
     }
 
     //sleep for time_Param
     sleep(time_Param);
 
     //join threads back to main
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < NUM_OF_PHIL; i++)
     {
-        pthread_join(&philThreads[i],NULL);
+        pthread_join(philThreads[i],NULL);
     }
 
 
     //release our semaphores and get each philosopher's meals had
     printf("Meals had: ");
     
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < NUM_OF_PHIL; i++)
     {
-        printf(" " + totalMeals[i]);
+        printf("Phil. %d : %d meals ",i,totalMeals[i]);
         sem_destroy(&chopstick[i]);
     }
 
@@ -84,15 +87,13 @@ int main(double time_Param)
 
 //Philosopher functions
 
-int philosopher(int PHIL_NUM)
+void *philosopher(void *PHIL_NUM)
 {
-    const int PHIL_NUM = PHIL_NUM;
-    
-
     //is meals subject to alteration?
     while(meals < MAX_MEALS)
     {
-
+        //after we check our while condition
+        sem_post(&mealAccess);
         
         pickup_forks(PHIL_NUM);
 
@@ -104,23 +105,23 @@ int philosopher(int PHIL_NUM)
         //spend time thinking
         sleep((rand() % (int)(MAX_ACTIVE_TIME)) + 1); //sleep between 1 and 4 seconds
 
-
+        //before we check our while condition
+        sem_wait(&mealAccess);
     }
 
 
-    return meals;
 }
 
-int pickup_forks(int PHIL_NUM)
+void pickup_forks(int* PHIL_NUM)
 {
     pthread_mutex_lock(&P_StatusMutex);
 
-    P_Status[PHIL_NUM] = HUNGRY;
+    P_Status[*PHIL_NUM] = HUNGRY;
 
     //what about PHIL 0? We do this to avoid out-of-bounds when PHIL 0
     //  grabs it's left then right, stick
-    int rightPHIL = (PHIL_NUM + 4) % 5;
-    int leftPHIL = (PHIL_NUM + 1) % 5;
+    int rightPHIL = (*PHIL_NUM + 4) % NUM_OF_PHIL;
+    int leftPHIL = (*PHIL_NUM + 1) % NUM_OF_PHIL;
 
     if(P_Status[rightPHIL] == EATING || P_Status[leftPHIL] == EATING)
     {
@@ -131,7 +132,7 @@ int pickup_forks(int PHIL_NUM)
 
     sem_wait(&chopstick[rightPHIL]);
     sem_wait(&chopstick[leftPHIL]);
-    P_Status[PHIL_NUM] = EATING;
+    P_Status[*PHIL_NUM] = EATING;
 
     pthread_mutex_unlock(&P_StatusMutex);
 
@@ -139,20 +140,25 @@ int pickup_forks(int PHIL_NUM)
     
 
 
-int return_forks(int PHIL_NUM)
+void return_forks(int *PHIL_NUM)
 {
     //what about PHIL 0? We do this to avoid out-of-bounds when PHIL 0
     //  grabs it's left then right, stick
-    int rightPHIL = (PHIL_NUM + 4) % 5;
-    int leftPHIL = (PHIL_NUM + 1) % 5;
+    int rightPHIL = (*PHIL_NUM + 4) % NUM_OF_PHIL;
+    int leftPHIL = (*PHIL_NUM + 1) % NUM_OF_PHIL;
 
     pthread_mutex_lock(&P_StatusMutex); 
 
     sem_post(&chopstick[rightPHIL]);
     sem_post(&chopstick[leftPHIL]);
 
-    P_Status[PHIL_NUM] = THINKING;
-    pthread_cond_signal(&P_StatusCond[PHIL_NUM]);
+    P_Status[*PHIL_NUM] = THINKING;
+    pthread_cond_signal(&P_StatusCond[*PHIL_NUM]);
+
+    sem_wait(&mealAccess);
+    meals++;
+    totalMeals[*PHIL_NUM]++;
+    sem_post(&mealAccess);
 
     pthread_mutex_unlock(&P_StatusMutex);
 }
