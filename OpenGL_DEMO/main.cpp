c48d7392ddfd4d7f340258fbb88f0431dc0a895e
LG <lmeg00001@gmail.com>
2025-01-16 00:44:40 -0500

    Bug corrections
    Fixed issue with LinkAttrib function for stride length and removed added values in main.cpp for vertices' colors

--- OpenGL_DEMO/main.cpp
+++ OpenGL_DEMO/main.cpp
@@ -9,17 +9,29 @@
 
 
 
-// Vertices coordinates
+// Test Vertices coordinates
 GLfloat vertices[] =
 {
 	//coords									// colors
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	0.0f, 0.8f, 0.3f, 0.02f,// Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		0.0f, 0.8f, 0.3f, 0.02f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  0.0f, 1.0f, 0.6f, 0.32f,// Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	0.0f, 0.9f, 0.45f, 0.17f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	0.0f, 0.9f, 0.45f, 0.17f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,		0.0f, 0.8f, 0.3f, 0.02f // Inner down
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	0.8f, 0.3f, 0.02f,// Lower left corner
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		0.8f, 0.3f, 0.02f, // Lower right corner
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  1.0f, 0.6f, 0.32f,// Upper corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	0.9f, 0.45f, 0.17f, // Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	0.9f, 0.45f, 0.17f, // Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,		0.8f, 0.3f, 0.02f // Inner down
 };
+
+// Vertices coordinates
+/*
+GLfloat vertices[] =
+{ //coords									    // colors
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+};*/
 
 // Indices for vertices order
 GLuint indices[] =
@@ -78,9 +90,9 @@ int main()
 	// Generates Element Buffer Object and links it to indices
 	EBO EBO1(indices, sizeof(indices));
 
-	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, sizeof(float), (void*)(3 * sizeof(float)));
+	// Links VBO to VAO, forgot to multiply stride
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
