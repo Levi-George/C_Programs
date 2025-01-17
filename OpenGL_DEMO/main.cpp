b5025a8a08f6b41e0f77e7b631961f4c616fecc5
LG <lmeg00001@gmail.com>
2025-01-16 19:53:48 -0500

    Square
    I made a square, commented out old triangle vertices

--- OpenGL_DEMO/main.cpp
+++ OpenGL_DEMO/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image/stb_image.h>
 
 #include"shaderClass.h"
 #include"VAO.h"
@@ -9,7 +10,8 @@
 
 
 
-// Test Vertices coordinates
+// Test Triangle Vertices coordinates
+/*
 GLfloat vertices[] =
 {
 	//coords									// colors
@@ -20,27 +22,33 @@ GLfloat vertices[] =
 	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	0.9f, 0.45f, 0.17f, // Inner right
 	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,		0.8f, 0.3f, 0.02f // Inner down
 };
+*/
 
-// Vertices coordinates
-/*
+// Test Square Vertices coordinates
 GLfloat vertices[] =
 { //coords									    // colors
-	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
-};*/
+	-0.5f, -0.5f, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	-0.5f, 0.5f, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.5f, 0.5f, 0.0f,     1.0f, 0.6f,  0.32f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     0.9f, 0.45f, 0.17f // Upper left corner
+};
 
-// Indices for vertices order
+// Indices for tirangle vertices order
+/*
 GLuint indices[] =
 {
 	0, 3, 5, // Lower left triangle
 	3, 2, 4, // Lower right triangle
 	5, 4, 1 // Upper triangle
 };
+*/
 
+//Indices for square vertices order
+GLuint indices[] =
+{
+	0, 2, 1, // Lower triangle
+	0, 3, 2 // Upper triangle
+};
 
 
 int main()
